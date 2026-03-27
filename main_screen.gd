fe5a11dd418c35e9da3bfc9b3bc37d28263a7a46
Amanshenoy008 <amanshenoy2002@gmail.com>
2026-03-27 17:42:49 -0400

    feat: add main menu screen

---
+++ main_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var new_cursor = preload("res://assets/new_cursor.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_custom_mouse_cursor(new_cursor) # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+	#pass # Replace with function body.
