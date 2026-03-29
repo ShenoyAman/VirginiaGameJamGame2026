d6d8441eeea96acb9fde58098bd0a452f45e822b
Amanshenoy008 <amanshenoy2002@gmail.com>
2026-03-29 13:57:00 -0400

    added music manager and audio system

---
+++ control_3backarrow.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var your_button = $BackArrow
+
+func _ready():
+	your_button.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+	GameState.label_visible = false
+	get_tree().change_scene_to_file("res://scenes/room1.tscn")
--- scenes/control.gd
+++ scenes/control.gd
@@ -6,4 +6,5 @@ func _ready():
 	your_button.pressed.connect(_on_button_pressed)
 
 func _on_button_pressed():
+	GameState.label_visible = false
 	get_tree().change_scene_to_file("res://room1right.tscn")
